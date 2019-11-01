2371debe9328dba86fc227f1e0aa90dd99fa58b2
p3kka123 <p0kemonisftw@hotmail.com>
2019-11-01 15:00:45 +0200

    changed dialog to state based

--- JunkCityRPG/Assets/DialogSystem/Dialog.cs
+++ JunkCityRPG/Assets/DialogSystem/Dialog.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 public class Dialog
 {
     string[] lines;
+    int index;
     int choices = 0;
     string[] choiceText;
+    DialogManager manager;
+    State state;
 
-    public Dialog(string[] lines)
-    {
-        this.lines = lines;
+    public enum State {
+        greet,
+        info,
+        quest,
+        end,
+
     }
 
-    public Dialog(string[] lines, int choices, string[] choiceText)
-    {
-        this.lines = lines;
-        this.choices = choices;
-        this.choiceText = choiceText;
+    public Dialog(DialogManager manager) {
+        this.manager = manager;
+        this.state = State.greet;
     }
 
-    public string GetLine(int index)
-    {
-        return lines[index];
-    }
+    public void NextLine() {
+        if (state == State.greet) {
+            manager.ST("Hey, you. You're finally awake. You were trying to cross the border, right? Can I help you with anything");
+            state = State.end;
+        } else if (state == State.info) {
 
-    public int GetLineAmount()
-    {
-        return lines.Length;
+        }else if(state == State.quest) {
+
+        }else if(state == State.end) {
+            manager.endConvo();
+            state = State.greet;
+        }
     }
 
 }
--- JunkCityRPG/Assets/DialogSystem/DialogManager.cs
+++ JunkCityRPG/Assets/DialogSystem/DialogManager.cs
@@ -10,34 +10,35 @@ public class DialogManager : MonoBehaviour, IPointerClickHandler
 {
 
     public Text text;
-    public TextAsset textFile;
     string[] lines;
     int index;
     Dialog dialog;
 
     void OnEnable()
     {
-        lines = Regex.Split(textFile.text, "\n");
-        dialog = new Dialog(lines);
-        text.text = dialog.GetLine(0);
-        index = 1;
+        //lines = Regex.Split(textFile.text, "\n");
+        dialog = new Dialog(this);
+        //text.text = dialog.GetLine(0);
+        //index = 1;
     }
 
     public void show()
     {
         this.gameObject.SetActive(true);
+        dialog.NextLine();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (index != dialog.GetLineAmount())
-        {
-            text.text = dialog.GetLine(index);
-            index++;
-        }
-        else
-        {
-            this.gameObject.SetActive(false);
-        }
+        dialog.NextLine();
     }
+
+    public void ST(string text) {
+        this.text.text = text;
+    }
+
+    public void endConvo() {
+        this.gameObject.SetActive(false);
+    }
+
 }
