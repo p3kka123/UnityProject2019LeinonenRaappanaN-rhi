1a836733a1a509ec5242ed5d2b55daa8fd0b577f
Lasse <34071995+p3kka123@users.noreply.github.com>
2019-11-01 02:21:17 +0200

    initial dialog class

--- JunkCityRPG/Assets/DialogSystem/Dialog.cs
+++ JunkCityRPG/Assets/DialogSystem/Dialog.cs
@@ -1,42 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Text.RegularExpressions;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class Dialog : MonoBehaviour, IPointerClickHandler
+public class Dialog
 {
-
-    public Text text;
-    public TextAsset textFile;
     string[] lines;
-    int index;
+    int choices = 0;
+    string[] choiceText;
 
-    void OnEnable()
+    public Dialog(string[] lines)
     {
-        lines = Regex.Split(textFile.text, "\n");
-        text.text = lines[0];
-        index = 1;
+        this.lines = lines;
     }
 
-    public void show()
+    public Dialog(string[] lines, int choices, string[] choiceText)
     {
-        this.gameObject.SetActive(true);
+        this.lines = lines;
+        this.choices = choices;
+        this.choiceText = choiceText;
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    public string GetLine(int index)
     {
-        if (index != lines.Length)
-        {
-            text.text = lines[index];
-            index++;
-        }
-        else
-        {
-            this.gameObject.SetActive(false);
-        }
-        //throw new System.NotImplementedException();
+        return lines[index];
     }
+
+    public int GetLineAmount()
+    {
+        return lines.Length;
+    }
+
 }
---
+++ JunkCityRPG/Assets/DialogSystem/DialogManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DialogManager : MonoBehaviour, IPointerClickHandler
+{
+
+    public Text text;
+    public TextAsset textFile;
+    string[] lines;
+    int index;
+    Dialog dialog;
+
+    void OnEnable()
+    {
+        lines = Regex.Split(textFile.text, "\n");
+        dialog = new Dialog(lines);
+        text.text = dialog.GetLine(0);
+        index = 1;
+    }
+
+    public void show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (index != dialog.GetLineAmount())
+        {
+            text.text = dialog.GetLine(index);
+            index++;
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
