b025fa217b44862a260c95ec8a231e32fb0dbbee
Richard Splenish <kimmo.r@windowslive.com>
2019-11-02 20:24:48 +0200

    Intro cutscene visuals done

---
+++ JunkCityRPG/Assets/Generic/ShipSway.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSway : MonoBehaviour
+{
+
+    public float perlinScale;
+    public float step;
+
+
+
+    private float x;
+    private float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler((Mathf.PerlinNoise(x,z) - 0.5f) * perlinScale,0,(Mathf.PerlinNoise(z+1000,x+1000) - 0.5f) * perlinScale);
+        print(Mathf.PerlinNoise(x,z)-0.5f + " " + (Mathf.PerlinNoise(z + 1000,x + 1000) - 0.5f));
+        z += step;
+        x += step;
+    }
+}
