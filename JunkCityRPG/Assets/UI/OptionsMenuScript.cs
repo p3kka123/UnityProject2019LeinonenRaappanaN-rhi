e41f4b32b1e7908c08e109608a39bc0f046d7c64
Lasse <34071995+p3kka123@users.noreply.github.com>
2019-12-02 17:06:17 +0200

    optionsmenu mainmenu

---
+++ JunkCityRPG/Assets/UI/OptionsMenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenuScript : MonoBehaviour
+{
+    
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+}
