b63afbed9edc792ecba0bb1ac0117ae48c0b2955
Lasse <34071995+p3kka123@users.noreply.github.com>
2019-12-04 19:22:05 +0200

    fix enemyaibase

---
+++ JunkCityRPG/Assets/Enemies/EnemyAiScriptBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyAiScriptBase : MonoBehaviour
+{
+    public virtual void PlayerDied()
+    {
+
+    }
+}
--- JunkCityRPG/Assets/Enemies/SlumAmbushThug/SlumAmbushThug.cs
+++ JunkCityRPG/Assets/Enemies/SlumAmbushThug/SlumAmbushThug.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SlumAmbushThug : AIScript
+public class SlumAmbushThug : EnemyAiScriptBase
 {
     private NavMeshAgent agent;
 
